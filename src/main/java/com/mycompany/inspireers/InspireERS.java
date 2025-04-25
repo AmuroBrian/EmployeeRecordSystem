a10ebe86aa35b2f0935fe5ec79783d59328f108b
Brian Perez <perezbrian.sti@gmail.com>
2025-04-25 18:17:43 +0800

    First Commit

---
+++ src/main/java/com/mycompany/inspireers/InspireERS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inspireers;
+
+/**
+ *
+ * @author amurobrian
+ */
+public class InspireERS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
