608e3dc90ebc0331f8b6f4fe465621887bb26b02
Brian Perez <perezbrian.sti@gmail.com>
2025-04-25 18:24:36 +0800

    Added JFrame

--- src/main/java/com/mycompany/inspireers/InspireERS.java
+++ src/main/java/com/mycompany/inspireers/InspireERS.java
@@ -11,6 +11,10 @@ package com.mycompany.inspireers;
 public class InspireERS {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new LoginFrame().setVisible(true);
+            }
+        });
     }
 }
